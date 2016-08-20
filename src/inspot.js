963b2f792bd6cd22e5b2a00570321927d3a34d82
정준영 <jjy920517@gmail.com>
2016-08-21 05:28:48 +0900

    current time 수정

--- src/inspot.js
+++ src/inspot.js
@@ -1,47 +1,70 @@
 console.log("javascript start =====>");
 
-var currentTime = new Date();
+var xmlHttp;
+
+function srvTime() {
+    console.log("Get server time");
+    if (window.XMLHttpRequest) {
+        xmlHttp = new XMLHttpRequest();
+        xmlHttp.open('HEAD', window.location.href.toString(), false);
+        xmlHttp.setRequestHeader("Content-Type", "text/html");
+        xmlHttp.send('');
+        return xmlHttp.getResponseHeader("Date");
+    } else if (window.ActiveXObject) {
+        xmlHttp = new ActiveXObject('Msxml2.XMLHTTP');
+        xmlHttp.open('HEAD', window.location.href.toString(), false);
+        xmlHttp.setRequestHeader("Content-Type", "text/html");
+        xmlHttp.send('');
+        return xmlHttp.getResponseHeader("Date");
+    }
+}
+var st = srvTime();
+
+var currentTime = new Date(st);
 
 function printCurrentYear() {
-  console.log("Year");
-  var yearID = document.getElementById("currentYear");
-  var yearName = currentTime.getFullYear();
-  yearID.innerHTML = yearName;
+    console.log("Year");
+    var yearID = document.getElementById("currentYear");
+    var yearName = currentTime.getFullYear();
+    yearID.innerHTML = yearName;
 }
 
 function printCurrentMonth() {
-  console.log("Month");
-  var monthID = document.getElementById("currentMonth");
-  var monthName = currentTime.getMonth();
-  monthID.innerHTML = monthName;
+    console.log("Month");
+    var arrMonthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+    var monthID = document.getElementById("currentMonth");
+    var monthName = currentTime.getMonth();
+    monthID.innerHTML = arrMonthNames[monthName];
 }
 
 function printCurrentDate() {
-  console.log("Day");
-  var dateID = document.getElementById("currentDate");
-  var dateName = currentTime.getDate();
-  dateID.innerHTML = dateName;
+    console.log("Date");
+    var dateID = document.getElementById("currentDate");
+    var dateName = currentTime.getDate();
+    dateID.innerHTML = dateName;
 }
 
 function printCurrentDay() {
-  console.log("Day");
-  var dayID = document.getElementById("currentDay");
-  var dayName = currentTime.getDay();
-  dayID.innerHTML = dayName;
+    console.log("Day");
+    var dayID = document.getElementById("currentDay");
+    var dayName = currentTime.getDay();
+    dayID.innerHTML = dayName;
 }
 
 function printCurrentHHMM() {
-  console.log("Day");
-  var hhmmID = document.getElementById("currentHHMM");
-  var hhmmName = currentTime.getDay();
-  hhmmID.innerHTML = hhmmName;
+    console.log("Day");
+    var hhmmID = document.getElementById("currentHHMM");
+    var hhmmName = currentTime.getDay();
+    hhmmID.innerHTML = hhmmName;
 }
 
-window.onload = function() {                         // 페이지가 로딩되면 실행
-  printCurrentYear();
-  printCurrentMonth();
-  printCurrentDate();
-  printCurrentDay();
+window.onload = function() { // 페이지가 로딩되면 실행
+    printCurrentYear();
+    printCurrentMonth();
+    printCurrentDate();
+    printCurrentDay();
 }
 
 /*
