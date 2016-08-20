bec1bc6c78ca52358e541077d5c6e95ed80a07f7
정준영 <jjy920517@gmail.com>
2016-08-21 08:25:48 +0900

    현재 날씨 추가

--- src/inspot.js
+++ src/inspot.js
@@ -85,18 +85,3 @@ window.onload = function() { // 페이지가 로딩되면 실행
     printCurrentDay();
     printCurrentHHMM();
 }
-
-/*
-document.write('현재 월: ' + (d.getMonth() + 1) + '<br />');
-document.write('현재 일: ' + d.getDate() + '<br />');
-
-document.write('<br />'); // 줄바꿈
-
-document.write('현재 시: ' + d.getHours() + '<br />');
-document.write('현재 분: ' + d.getMinutes() + '<br />');
-document.write('현재 초: ' + d.getSeconds() + '<br />');
-
-document.write('<br />');
-
-document.write('오늘 요일: ' + d.getDay() + '<br />'); // 일요일 = 0
-*/
