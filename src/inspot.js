80928b87572a62f4bd89f7f4896bccb2d13ad5ef
정준영 <jjy920517@gmail.com>
2016-08-21 13:45:22 +0900

    링크 수정

--- src/inspot.js
+++ src/inspot.js
@@ -81,14 +81,14 @@ function printCurrentHHMM() {
 function settingButtonEvent() {
   var settingButton = document.getElementById("settingButton");
   settingButton.onclick = function() {
-    location.replace("/var/www/html/setting.html");
+    location.replace("setting.html");
   }
 }
 
 function logoButtonEvent() {
   var logoButton = document.getElementById("logo");
   logoButton.onclick = function() {
-    location.replace("/var/www/html/index.html");
+    location.replace("index.html");
   }
 }
 
