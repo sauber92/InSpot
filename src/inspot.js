a870ee83758303545df60663e7575cfd924ce8de
정준영 <jjy920517@gmail.com>
2016-08-21 10:07:12 +0900

    javascript 분

--- src/inspot.js
+++ src/inspot.js
@@ -2,6 +2,10 @@ console.log("javascript start =====>");
 
 var xmlHttp;
 
+var n_name = document.getElementById("p_name").firstChild;
+var n_clouds = document.getElementById("p_clouds").firstChild;
+var n_temp = document.getElementById("p_temp").firstChild;
+
 function srvTime() {
     console.log("Get server time");
     if (window.XMLHttpRequest) {
@@ -78,10 +82,62 @@ function printCurrentHHMM() {
     hhmmID.innerHTML = hhmmName;
 }
 
+function getWeather() {
+  var apiURL = "http://api.openweathermap.org/data/2.5/weather?q=Seoul&APPID=e42202e66f1f003b5aa38a48f7ddbb87";
+  var f_name;
+  var f_temp;
+  var f_weather;
+  var f_clouds;
+
+  $.ajax({
+    console.log("ajax start ===>");
+    url: apiURL,
+    dataType: "json",
+    type: "GET",
+    async: "false",
+    success: function(forecast) {
+      console.log("function success!");
+      f_name = forecast.name;
+      console.log("name success!");
+      f_temp = (forecast.main.temp-273.15).toFixed(0);
+      console.log("clouds success!");
+      f_weather = forecast.weather[0].id;
+      console.log("weather success!");
+      f_clouds = forecast.clouds.all;
+      console.log("clouds success!");
+      n_name.replaceData(0, 10, f_name);
+      console.log("name change success!");
+      n_clouds.replaceData(4, 2, f_clouds);
+      console.log("clouds change success!");
+      n_temp.replaceData(0, 2, f_temp);
+      console.log("temp change success!");
+
+      console.log("if!");
+
+      if(f_weather == 800) {
+        document.getElementById("png").src="/src/img/clearsky.png";
+      }
+      else if(700 <= f_weather < 800) {
+        document.getElementById("png").src="/src/img/fewclouds.png";
+      }
+      else if(800 < f_weather < 900) {
+        document.getElementById("png").src="/src/img/clouds.png";
+      }
+      else {
+        document.getElementById("png").src="/src/img/rain.png";
+      }
+    },
+    error: function(forecast) {
+      alert("error: " + e);
+    }
+  });
+}
+
 window.onload = function() { // 페이지가 로딩되면 실행
     printCurrentYear();
     printCurrentMonth();
     printCurrentDate();
     printCurrentDay();
     printCurrentHHMM();
+    getWeather();
 }
