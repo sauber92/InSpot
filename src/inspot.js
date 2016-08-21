b9ee1939bcabe0edb127228de00b3af431b5c657
정준영 <jjy920517@gmail.com>
2016-08-21 13:39:38 +0900

    setting page 수정 / 로고 클릭 시 메인 이동 수정

--- src/inspot.js
+++ src/inspot.js
@@ -88,7 +88,7 @@ function settingButtonEvent() {
 function logoButtonEvent() {
   var logoButton = document.getElementById("logo");
   logoButton.onclick = function() {
-    location.replace("163.180.142.81");
+    location.href="163.180.142.81";
   }
 }
 
